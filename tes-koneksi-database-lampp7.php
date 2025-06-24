a7c7c868bca80fdd53bca6b9fc978940c34cc093
otnamrehus <otnamreheos@gmail.com>
2025-06-24 16:04:06 +0700

    Update tes-koneksi-database-lampp7.php

--- tes-koneksi-database-lampp7.php
+++ tes-koneksi-database-lampp7.php
@@ -1,6 +1,4 @@
 <?php
-//$host = 'mariadb';      // atau 'mariadb' jika pakai mengarah ke container MariaDB - PHP8.0
-// atau 
 $host = 'mysql';      // atau 'mariadb' jika pakai mengarah ke container MySQL - PHP7.4 
 $dbname = 'mysql';
 $username = 'root';
@@ -10,8 +8,8 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // Set error mode ke exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo " <h1>Koneksi ke database berhasil!</h1>";
+    echo " <h1>Koneksi ke Database Lampp 7 Berhasil!</h1>";
 } catch (PDOException $e) {
-    echo " <h1>Koneksi gagal: </h1>" . $e->getMessage();
+    echo " <h1>Koneksi Database Lampp 7 Gagal: </h1>" . $e->getMessage();
 }
 ?>
