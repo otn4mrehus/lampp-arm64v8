0f9f2ba26ae7943ab2254e1609aa86b1af8a1990
otnamrehus <otnamreheos@gmail.com>
2025-06-24 10:18:49 +0700

    Update service-lampp.sh

--- service-lampp.sh
+++ service-lampp.sh
@@ -15,6 +15,7 @@ submenu_php7() {
     echo "2. Stop"
     echo "3. Restart"
     echo "4. Down (stop & hapus kontainer/volume)"
+    echo "5. Aktifkan SSH (PermitRootLogin) di PHP 7.4"
     echo "0. Kembali"
 }
 
@@ -24,6 +25,7 @@ submenu_php8() {
     echo "2. Stop"
     echo "3. Restart"
     echo "4. Down (stop & hapus kontainer/volume)"
+    echo "5. Aktifkan SSH (PermitRootLogin) di PHP 8.0"
     echo "0. Kembali"
 }
 
@@ -34,7 +36,7 @@ while true; do
         1)
             while true; do
                 submenu_php7
-                read -p "Pilih aksi untuk PHP 7.4 + MySQL 5.7 [0-4]: " aksi7
+                read -p "Pilih aksi untuk PHP 7.4 + MySQL 5.7 [0-5]: " aksi7
                 case $aksi7 in
                     1)
                         echo "Menjalankan PHP 7.4 + MySQL 5.7 + phpMyAdmin..."
@@ -52,6 +54,10 @@ while true; do
                         echo "Down (stop & hapus kontainer/volume) PHP 7.4 + MySQL 5.7..."
                         docker-compose -f "$COMPOSE_FILE" -p 'lampp7' down -v
                         ;;
+                    5)
+                        echo "Mengaktifkan SSH di lampp-php7.4..."
+                        docker exec -it lampp-php7.4 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && echo "roottoor1" | chpasswd && service ssh restart'
+                        ;;
                     0)
                         break
                         ;;
@@ -65,7 +71,7 @@ while true; do
         2)
             while true; do
                 submenu_php8
-                read -p "Pilih aksi untuk PHP 8 + MariaDB 10.5 [0-4]: " aksi8
+                read -p "Pilih aksi untuk PHP 8 + MariaDB 10.5 [0-5]: " aksi8
                 case $aksi8 in
                     1)
                         echo "Menjalankan PHP 8 + MariaDB 10.5 + phpMyAdmin..."
@@ -82,6 +88,10 @@ while true; do
                     4)
                         echo "Down (stop & hapus kontainer/volume) PHP 8 + MariaDB 10.5..."
                         docker-compose -f "$COMPOSE_FILE" -p 'lampp8' down -v
+                        ;;
+                    5)
+                        echo "Mengaktifkan SSH di lampp-php8.0..."
+                        docker exec -it lampp-php8.0 bash -c 'echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && echo "root:toor" | chpasswd && service ssh restart'
                         ;;
                     0)
                         break
