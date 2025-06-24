f9866fbb5d63180102d9ded1c146c1e69b201dcd
otnamrehus <otnamreheos@gmail.com>
2025-06-24 16:04:45 +0700

    Update tes-koneksi-database-lampp8.php

--- tes-koneksi-database-lampp8.php
+++ tes-koneksi-database-lampp8.php
@@ -8,8 +8,8 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     // Set error mode ke exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo " <h1>Koneksi ke database Lampp 8 berhasil!</h1>";
+    echo " <h1>Koneksi ke Database Lampp 8 Berhasil!</h1>";
 } catch (PDOException $e) {
-    echo " <h1>Koneksi Lampp 8 gagal: </h1>" . $e->getMessage();
+    echo " <h1>Koneksi Database Lampp 8 Gagal: </h1>" . $e->getMessage();
 }
 ?>
